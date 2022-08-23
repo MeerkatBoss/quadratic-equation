6d933ca7c29c2bd977f07c04db7df838605f4eeb
MeerkatBoss <ivan.solodovnikov@outlook.com>
2022-08-23 18:10:30 +0300

    Add Doxygen documentation

--- src/quadio.h
+++ src/quadio.h
@@ -1,13 +1,65 @@
 #ifndef QUADIO_H
 #define QUADIO_H
 
+/**
+ * @file quadio.h
+ * @author MeerkatBoss
+ * @brief Contains definitions for IO functions
+ * @version 0.1
+ * @date 2022-08-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <stdio.h>
 #include "quadratic.h"
 
+/**
+ * @brief Displays help message to stdout
+ */
 void show_help(void);
+
+/**
+ * @brief Reads and discards all symbols until '\\n'
+ * from stream
+ * 
+ * @param fd [in] - file descriptor of a stream
+ * @return 0 upon success; EOF upon encountering end-of-file
+ */
 int flush_input(FILE *fd);
-int parse_args(int, char**, ...);
-int interactive_input(double*, double*, double*);
-int print_solutions(enum root_count, double, double);
+
+/**
+ * @brief Reads double-precision floating-point numbers
+ * from string
+ * 
+ * @param n [in] - amount of numbers to read
+ * @param str [in] - string, containing numbers
+ * @param ... [out] - pointers for storing numbers
+ * @return 0 upon success; -2 upon failure
+ */
+int parse_args(int n, char** str, ...);
+
+/**
+ * @brief Reads three double-precision floating-point numbers
+ * interactively
+ * 
+ * @param a [out] - pointer to first number
+ * @param b [out] - pointer to second number
+ * @param c [out] - pointer to third number
+ * @return 0 upon success, EOF upon encountering end-of-file,
+ * -2 upon failure 
+ */
+int interactive_input(double* a, double* b, double* c);
+
+/**
+ * @brief Produces formatted output of equation roots
+ * 
+ * @param n_roots [in] - value, representing number of roots
+ * @param x1 [in] - first root
+ * @param x2 [in] - second root
+ * @return 0 upon success, -2 upon failure
+ */
+int print_solutions(enum root_count n_roots, double x1, double x2);
 
 #endif
--- src/quadratic.h
+++ src/quadratic.h
@@ -1,16 +1,69 @@
 #ifndef QUADRATIC_H
 #define QUADRATIC_H
 
+/**
+ * @file quadratic.h
+ * @author MeerkatBoss
+ * @brief Contains definitions for equation solvers
+ * @version 0.1
+ * @date 2022-08-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+/**
+ * @brief Enum representing number of roots
+ * of an equation
+ */
 enum root_count
 {
-    NO_ROOTS    =  0,
-    SINGLE_ROOT =  1,
-    TWO_ROOTS   =  2,
-    INF_ROOTS   = -1
+    NO_ROOTS    =  0, /*!< Equation has no roots */
+    SINGLE_ROOT =  1, /*!< Equation has only one root */
+    TWO_ROOTS   =  2, /*!< Equation has two distinct roots */
+    INF_ROOTS   = -1  /*!< Any real number is a solution */
 };
 
-int compare_double(double, double);
-enum root_count solve_quadratic(double, double, double, double*, double*);
-enum root_count solve_linear(double, double, double*);
+/**
+ * @brief Compares two double-precision floating-point numbers
+ * 
+ * @param a [in] - first number
+ * @param b [in] - second number
+ * @return negative number if a < b; positive number if a > b;
+ * 0 if a and b are close enough to be considered equal
+ * 
+ * @note Minimal difference between numbers considered not equal is 1e-9
+ */
+int compare_double(double a, double b);
+
+/**
+ * @brief Solves equations ax^2 + bx + c = 0
+ * 
+ * @param a [in] - first coefficient of an equaion
+ * @param b [in] - second coefficient of an equation
+ * @param c [in] - third coefficient of an equation
+ * @param x1 [out] - pointer to the lesser of the roots
+ * @param x2 [out] - pointer to the bigger of the roots
+ * @return value, representing the number of roots
+ * 
+ * @note If there is only one solution to the equation, x2 is not changed.
+ * If there are no roots or an infinite amount of roots,
+ * both x1 and x2 are not changed.
+ * If there are two roots, *x1 < *x2
+ */
+enum root_count solve_quadratic(double a, double b, double c,
+                                double *x1, double *x2);
+
+/**
+ * @brief Solves equations ax + b = 0
+ * 
+ * @param a [in] - first coefficient of an equation
+ * @param b [in] - second coefficient of an equation
+ * @param x [out] - pointer to the root
+ * @return value, representing the number of roots
+ * 
+ * @note If there are no roots or an infinite amount of roots, x is not changed
+ */
+enum root_count solve_linear(double a, double b, double* x);
 
 #endif
